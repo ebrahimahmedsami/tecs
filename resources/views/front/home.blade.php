da641c4ca9902968999927029cb76a2b1d30cbfd
EbrahimSami <ebrahim.sami@roqay.com>
2023-01-08 02:16:48 +0200

    lang

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -55,8 +55,8 @@
 <!-- Masthead-->
 <header class="masthead">
     <div class="container">
-        <div class="masthead-subheading">{{isset($settings->header) ? strtoupper($settings->header) : '--'}}</div>
-        <div class="masthead-heading text-uppercase">{{isset($settings->about) ? ucfirst($settings->about) : '--'}}</div>
+        <div class="masthead-subheading text-black">{{isset($settings->header) ? strtoupper($settings->header) : '--'}}</div>
+        <div class="masthead-heading text-uppercase text-black">{{isset($settings->about) ? ucfirst($settings->about) : '--'}}</div>
         <a class="btn btn-primary btn-xl text-uppercase" id="reserve-modal" data-bs-toggle="modal" data-bs-target="#exampleModal">{{__('home.reserve with us')}}</a>
     </div>
 </header>
@@ -69,7 +69,7 @@
     <div class="container">
         <div class="text-center">
             <h2 class="section-heading text-uppercase">@lang('dashboard.service')</h2>
-            <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+            <h3 class="section-subheading text-muted">كل ما يخص الخدمات وما تقدمه الشركه</h3>
         </div>
         <div class="row text-center">
             <div class="col-md-4">
@@ -77,24 +77,28 @@
                             <i class="fas fa-circle fa-stack-2x text-primary"></i>
                             <i class="fas fa-shopping-cart fa-stack-1x fa-inverse"></i>
                         </span>
-                <h4 class="my-3">E-Commerce</h4>
-                <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+                <h4 class="my-3">الإعلانات والتسويق</h4>
+                <p class="text-muted">تعتبر الإعلانات أحد جوانب التسويق؛ وعلى الرغم من أن عملية التسويق تعتبر نهجاً كلياً يمكن من خلاله التحدث مع العملاء عن علامتك التجارية، عادة ما يكون الإعلان شكلاً مدفوعاً من أشكال الرسائل المصممة لتشجيع المبيعات؛ وقد تكون الإعلانات مقدمة على المدى القصير للترويج لعروض خاصة أو مستمرة، إلا أنها عادة ما تحتاج إلى استثمار مالي؛ وإذا كنت على علم بأن الإعلانات قد تقدم المساعدة لمشروعك ولكن غير متأكد من النقطة التي يتعين عليك البدء منها، اطلع على نصائح التسويق الواردة أدناه للتعرف على الخيارلإحتياجاتك الأنسب لاحتياجات وموازنتك.</p>
             </div>
             <div class="col-md-4">
                         <span class="fa-stack fa-4x">
                             <i class="fas fa-circle fa-stack-2x text-primary"></i>
                             <i class="fas fa-laptop fa-stack-1x fa-inverse"></i>
                         </span>
-                <h4 class="my-3">Responsive Design</h4>
-                <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+                <h4 class="my-3">تصميم اللوجوهات والبانر</h4>
+                <p class="text-muted">هو الهوية المميزة لاي مشروع او منشأة تجارية . وهو يعبر عن طبيعة وماهية هذه المنشأة ويعطي لمحة عنها وشعار الموقع أول ما يلفت نظر العميل أو زائر الموقع . حيث أن الشعار كلما كان متميزاً ومتناسقاً مع طبيعة المنشأة كلما كان له تأثير أكبر لدى الزبائن والعملاء وليس المهم فقط أن يتميز المصمم باتقانه للعمل على برامج التصميم المختلفة وانما الاهم ان يكون لديه المقدرة على التفكير والابداع وتشكيل الفكرة المناسبة لخلق شعار يتوافق مع طبيعة نشاط الموقع . وهذا ما يتطلبه تصميم شعار او لوجو ناجح . وحياة هوست من هذا المنطلق تقدم هذه الخدمة لعملائها في تصميم الشعارات للمواقع والمؤسسات معتمدة على فريق مصممين مبدع قادر على خلق الفكرة وابتكارها وتطبيقها بصورة أفضل من المتوقع.</p>
             </div>
             <div class="col-md-4">
                         <span class="fa-stack fa-4x">
                             <i class="fas fa-circle fa-stack-2x text-primary"></i>
                             <i class="fas fa-lock fa-stack-1x fa-inverse"></i>
                         </span>
-                <h4 class="my-3">Web Security</h4>
-                <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+                <h4 class="my-3">تصميم التطبيقات والمواقع</h4>
+                <p class="text-muted">صميم المواقع او تصميم مواقع الانترنت هو عملية تجميع عناصر وتخطيط محتويات الوسائط المتعددة بواسطه عدة لغات وبرامج لاخراج تصميم مناسب للعرض
+
+                    على متصفحات الانترنت بطريقة صحيحة وسهلة الاستخدام لرواد وبالتالى يكون الموقع قد ادى الغرض الذى انشأ من اجله حيث يحتاج كل موقع لـ
+
+                    استضافة المواقع الالكترونية لكي تضع بيانات الموقع في مساحة تخزين تمتاز بالحماية والامان لحماية بياناتك من السرقة من قبل قرصنة الانترنت.</p>
             </div>
         </div>
     </div>
@@ -123,110 +127,11 @@
 <section class="page-section" id="about">
     <div class="container">
         <div class="text-center">
-            <h2 class="section-heading text-uppercase">About</h2>
-            <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+            <h2 class="section-heading text-uppercase">عنا</h2>
+            <h3 class="section-subheading text-muted">شركه TECS كل ما يخص التكنولوجيا</h3>
         </div>
-{{--        <ul class="timeline">--}}
-{{--            <li>--}}
-{{--                <div class="timeline-image"><img class="rounded-circle img-fluid" src="{{asset('frontAssets/assets/img/about/1.jpg')}}" alt="..." /></div>--}}
-{{--                <div class="timeline-panel">--}}
-{{--                    <div class="timeline-heading">--}}
-{{--                        <h4>2009-2011</h4>--}}
-{{--                        <h4 class="subheading">Our Humble Beginnings</h4>--}}
-{{--                    </div>--}}
-{{--                    <div class="timeline-body"><p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p></div>--}}
-{{--                </div>--}}
-{{--            </li>--}}
-{{--            <li class="timeline-inverted">--}}
-{{--                <div class="timeline-image"><img class="rounded-circle img-fluid" src="{{asset('frontAssets/assets/img/about/2.jpg')}}" alt="..." /></div>--}}
-{{--                <div class="timeline-panel">--}}
-{{--                    <div class="timeline-heading">--}}
-{{--                        <h4>March 2011</h4>--}}
-{{--                        <h4 class="subheading">An Agency is Born</h4>--}}
-{{--                    </div>--}}
-{{--                    <div class="timeline-body"><p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p></div>--}}
-{{--                </div>--}}
-{{--            </li>--}}
-{{--            <li>--}}
-{{--                <div class="timeline-image"><img class="rounded-circle img-fluid" src="{{asset('frontAssets/assets/img/about/3.jpg')}}" alt="..." /></div>--}}
-{{--                <div class="timeline-panel">--}}
-{{--                    <div class="timeline-heading">--}}
-{{--                        <h4>December 2015</h4>--}}
-{{--                        <h4 class="subheading">Transition to Full Service</h4>--}}
-{{--                    </div>--}}
-{{--                    <div class="timeline-body"><p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p></div>--}}
-{{--                </div>--}}
-{{--            </li>--}}
-{{--            <li class="timeline-inverted">--}}
-{{--                <div class="timeline-image"><img class="rounded-circle img-fluid" src="{{asset('frontAssets/assets/img/about/4.jpg')}}" alt="..." /></div>--}}
-{{--                <div class="timeline-panel">--}}
-{{--                    <div class="timeline-heading">--}}
-{{--                        <h4>July 2020</h4>--}}
-{{--                        <h4 class="subheading">Phase Two Expansion</h4>--}}
-{{--                    </div>--}}
-{{--                    <div class="timeline-body"><p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p></div>--}}
-{{--                </div>--}}
-{{--            </li>--}}
-{{--            <li class="timeline-inverted">--}}
-{{--                <div class="timeline-image">--}}
-{{--                    <h4>--}}
-{{--                        Be Part--}}
-{{--                        <br />--}}
-{{--                        Of Our--}}
-{{--                        <br />--}}
-{{--                        Story!--}}
-{{--                    </h4>--}}
-{{--                </div>--}}
-{{--            </li>--}}
-{{--        </ul>--}}
     </div>
 </section>
-<!-- Team-->
-{{--<section class="page-section bg-light" id="clinics">--}}
-{{--    <div class="container">--}}
-{{--        <div class="text-center">--}}
-{{--            <h2 class="section-heading text-uppercase">Our Amazing Team</h2>--}}
-{{--            <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>--}}
-{{--        </div>--}}
-{{--        <div class="row">--}}
-{{--            <div class="col-lg-4">--}}
-{{--                <div class="team-member">--}}
-{{--                    <img class="mx-auto rounded-circle" src="{{asset('frontAssets/assets/img/team/1.jpg')}}" alt="..." />--}}
-{{--                    <h4>Parveen Anand</h4>--}}
-{{--                    <p class="text-muted">Lead Designer</p>--}}
-{{--                    <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Parveen Anand Twitter Profile"><i class="fab fa-twitter"></i></a>--}}
-{{--                    <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Parveen Anand Facebook Profile"><i class="fab fa-facebook-f"></i></a>--}}
-{{--                    <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Parveen Anand LinkedIn Profile"><i class="fab fa-linkedin-in"></i></a>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--            <div class="col-lg-4">--}}
-{{--                <div class="team-member">--}}
-{{--                    <img class="mx-auto rounded-circle" src="{{asset('frontAssets/assets/img/team/2.jpg')}}" alt="..." />--}}
-{{--                    <h4>Diana Petersen</h4>--}}
-{{--                    <p class="text-muted">Lead Marketer</p>--}}
-{{--                    <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Diana Petersen Twitter Profile"><i class="fab fa-twitter"></i></a>--}}
-{{--                    <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Diana Petersen Facebook Profile"><i class="fab fa-facebook-f"></i></a>--}}
-{{--                    <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Diana Petersen LinkedIn Profile"><i class="fab fa-linkedin-in"></i></a>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--            <div class="col-lg-4">--}}
-{{--                <div class="team-member">--}}
-{{--                    <img class="mx-auto rounded-circle" src="{{asset('frontAssets/assets/img/team/3.jpg')}}" alt="..." />--}}
-{{--                    <h4>Larry Parker</h4>--}}
-{{--                    <p class="text-muted">Lead Developer</p>--}}
-{{--                    <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Larry Parker Twitter Profile"><i class="fab fa-twitter"></i></a>--}}
-{{--                    <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Larry Parker Facebook Profile"><i class="fab fa-facebook-f"></i></a>--}}
-{{--                    <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Larry Parker LinkedIn Profile"><i class="fab fa-linkedin-in"></i></a>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--        <div class="row">--}}
-{{--            <div class="col-lg-8 mx-auto text-center"><p class="large text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut eaque, laboriosam veritatis, quos non quis ad perspiciatis, totam corporis ea, alias ut unde.</p></div>--}}
-{{--        </div>--}}
-{{--    </div>--}}
-{{--</section>--}}
-<!-- Clients-->
-<!-- Contact-->
 <section class="page-section" id="contact">
     <div class="container">
         <div class="text-center">
